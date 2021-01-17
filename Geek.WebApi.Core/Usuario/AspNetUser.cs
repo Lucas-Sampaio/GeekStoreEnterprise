65bb838aa2b5b8c2f1303cc5c82ff8d212e44c12
lucas.souza <lucas.souza@opensystem.srv.br>
2021-01-16 21:52:53 -0300

    Alterado classe User para o core

---
+++ Geek.WebApi.Core/Usuario/AspNetUser.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace Geek.WebApi.Core.Usuario
+{
+    public class AspNetUser : IAspNetUser
+    {
+        private readonly IHttpContextAccessor _accessor;
+        public AspNetUser(IHttpContextAccessor acessor)
+        {
+            _accessor = acessor;
+        }
+
+        public string Name => _accessor.HttpContext.User.Identity.Name;
+        public Guid ObterUserId()
+        {
+            var guid = EstaAutenticado() ? Guid.Parse(_accessor.HttpContext.User.GetUserId()) : Guid.Empty;
+            return guid;
+        }
+
+        public string ObterUserEmail()
+        {
+            var email = EstaAutenticado() ? _accessor.HttpContext.User.GetUserEmail() : "";
+            return email;
+        }
+
+        public string ObterUserToken()
+        {
+            var token = EstaAutenticado() ? _accessor.HttpContext.User.GetUserToken() : "";
+            return token;
+        }
+
+        public bool EstaAutenticado()
+        {
+            return _accessor.HttpContext.User.Identity.IsAuthenticated;
+        }
+
+        public bool PossuiRole(string role)
+        {
+            return _accessor.HttpContext.User.IsInRole(role);
+        }
+
+        public IEnumerable<Claim> ObterClaims()
+        {
+            return _accessor.HttpContext.User.Claims;
+        }
+
+        public HttpContext ObterHttpContext()
+        {
+            return _accessor.HttpContext;
+        }
+    }
+
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string GetUserId(this ClaimsPrincipal principal)
+        {
+            if (principal == null)
+            {
+                throw new ArgumentException(nameof(principal));
+            }
+
+            var claim = principal.FindFirst(ClaimTypes.NameIdentifier);
+            return claim?.Value;
+        }
+
+        public static string GetUserEmail(this ClaimsPrincipal principal)
+        {
+            if (principal == null)
+            {
+                throw new ArgumentException(nameof(principal));
+            }
+            var claim = principal.FindFirst(ClaimTypes.Email);
+            return claim?.Value;
+        }
+        public static string GetUserToken(this ClaimsPrincipal principal)
+        {
+            if (principal == null)
+            {
+                throw new ArgumentException(nameof(principal));
+            }
+            var claim = principal.FindFirst("JWT");
+            return claim?.Value;
+        }
+    }
+}
+
