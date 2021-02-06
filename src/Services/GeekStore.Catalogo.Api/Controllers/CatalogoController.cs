7d2c2b79745a7d8f501208ae12a108c96c7ce109
lucas.souza <lucas.souza@opensystem.srv.br>
2021-02-05 22:42:24 -0300

    configurado api catalogo

--- src/Services/GeekStore.Catalogo.Api/Controllers/CatalogoController.cs
+++ src/Services/GeekStore.Catalogo.Api/Controllers/CatalogoController.cs
@@ -1,7 +1,5 @@
 ﻿using Geek.WebApi.Core.Controller;
-using Geek.WebApi.Core.Identidade;
 using GeekStore.Catalogo.Api.Model;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace GeekStore.Catalogo.Api.Controllers
 {
-    [Route("api/[controller]")] 
+    [Route("api/[controller]")]
     public class CatalogoController : MainController
     {
         private readonly IProdutoRepository _produtoRepository;
@@ -19,11 +17,11 @@ namespace GeekStore.Catalogo.Api.Controllers
             _produtoRepository = produtoRepository;
         }
 
-      
+
         [HttpGet("produtos")]
         public async Task<PagedResult<Produto>> Index([FromQuery] int pageSize = 8, [FromQuery] int page = 1, [FromQuery] string q = null)
         {
-            return await _produtoRepository.ObterTodos(pageSize,page,q);
+            return await _produtoRepository.ObterTodos(pageSize, page, q);
         }
 
         //[ClaimsAuthorize("Catalogo", "Ler")]
